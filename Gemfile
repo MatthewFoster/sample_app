1036768a7af5d6dcac165730ab0d14761295ee48
Matthew Foster <matafoster@gmail.com>
2012-04-16 09:20:18 -0400

    removing the default page

--- Gemfile
+++ Gemfile
@@ -1,13 +1,15 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.1'
+gem 'bootstrap-sass', '2.0.0'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-group :development,:test do
+group :development do
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.8.1'
+  gem 'guard-rspec', '0.5.5'
 end
 
 
@@ -26,7 +28,10 @@ end
 gem 'jquery-rails', '2.0.0'
 
 group :test do
+  gem 'rspec-rails', '2.8.1'
   gem 'capybara', '1.1.2'
+  gem 'rb-fsevent', '0.4.3.1', :require => false
+  gem 'growl', '1.0.3'
 end
 
 group :production do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,42 +2,57 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
+      visit root_path
+      page.should have_selector('h1', text: 'Sample App')
     end
 
     it "should have the right title" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => "| Home")
+      visit root_path
+      page.should have_selector('title', text: "#{base_title} | Home")
     end
   end
 
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
+      visit help_path
+      page.should have_selector('h1', text: 'Help')
     end
 
     it "should have the right title" do
-      visit '/static_pages/help'
-      page.should have_selector('title', :text => "| Help")
+      visit help_path
+      page.should have_selector('title', text: "#{base_title} | Help")
     end
   end
 
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
+      visit about_path
+      page.should have_selector('h1', text: 'About Us')
     end
 
     it "should have the right title" do
-      visit '/static_pages/about'
-      page.should have_selector('title', :text => "| About Us")
+      visit about_path
+      page.should have_selector('title', text: "#{base_title} | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "should have the right title" do
+      visit contact_path
+      page.should have_selector('title', text: "#{base_title} | Contact")
     end
   end
 
